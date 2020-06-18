0255eba5add5a7b702995b15de418cafa065433d
Karlus44 <charles.havez@gmail.com>
2020-06-18 19:11:11 +0200

    link in Table.js

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -106,7 +106,7 @@ class Table extends Component {
           : null}
               {this.props.type==='files-prof' || this.props.type==='files-admin' || this.props.type==='files-eleve' ?
                 <td>
-                    <a href = {path.join(Liste[rowIndex].Lien, Liste[rowIndex].Nom)}  download
+                    <a href = {path.join('..',Liste[rowIndex].Lien, Liste[rowIndex].Nom)}  download
                       className="bg-light-green br4 f6 link dim black pa0 ph2 mt0 mv0 b--black">Ouvrir</a>
                   </td>
             : null}
